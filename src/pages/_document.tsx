da6a91e27e72bfe35f673decc8aa3e0894de7b86
Paulo Cesar <paulocitron99@gmail.com>
2021-07-13 15:23:11 -0300

    feat: add material-ui

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -7,8 +7,8 @@ export default class MyDocument extends Document {
         <Head>
           <link rel="preconnect" href="https://fonts.gstatic.com" />
           <link
-            href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;500;700&display=swap"
             rel="stylesheet"
+            href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap"
           />
         </Head>
         <body>
